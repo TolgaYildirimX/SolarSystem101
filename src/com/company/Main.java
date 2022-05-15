0fdc287624eef5a154c609a13d21a33ae5a7502f
tolga <tolgayyildirim87@gmail.com>
2022-05-15 13:19:39 -0500

    Solar System Basic Ready

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /**
+         * branching
+         * create basic structure of solar system and deploy asap
+         */
+
+        Solarsystem factorX = new Solarsystem();
+        factorX.sun="1";
+        factorX.moon="4";
+        factorX.planet="2";
+        factorX.stars="1000";
+        
+
+    }
+}
---
+++ src/com/company/Solarsystem.java
@@ -0,0 +1,10 @@
+package com.company;
+
+public class Solarsystem {
+
+    String sun;
+    String moon;
+    String planet;
+    String stars;
+
+}
